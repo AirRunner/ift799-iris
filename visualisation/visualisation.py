042055a3ba276f56bcadc4b072d82871c1e26f88
AirRunner <luca.vaio@efrei.net>
2021-09-28 22:07:54 -0400

    Update histograms appearance

--- visualisation/visualisation.py
+++ visualisation/visualisation.py
@@ -1,21 +1,29 @@
-def hist_plot(df, var, cats='species'):
+def hist_plot(df, var, cats='species', pairs=True):
     from itertools import combinations
     from operator import itemgetter
     import matplotlib.pyplot as plt
     from seaborn import histplot, color_palette
+
+    if pairs:
+        colours = ["blue", "orange", "green"]
+        palette = dict(zip(colours, color_palette()[0:3]))
+        col = lambda c1, c2: itemgetter(c1, c2)(palette)
+        species = list(df[cats].unique())
+        species.reverse()
+        
+        fig, axs = plt.subplots(1, 3, figsize=(16, 5))
+        fig.suptitle(f"Pair classes distribution for {var}")
+
+        for i, (cla, cols) in enumerate(zip(species, combinations(colours, 2))):
+            histplot(
+                df[df[cats] != cla], x=var, multiple="stack", hue=cats,
+                bins=20, shrink=0.5, ax=axs[i], palette=col(*cols)
+            )
+    else:
+        fig = plt.figure(figsize=(8, 5))
+        histplot(df, x=var, multiple="stack", hue=cats, bins=30, shrink=0.5)
+        plt.title(f"Classes distribution for {var}")
     
-    colours = ["blue", "orange", "green"]
-    palette = dict(zip(colours, color_palette()[0:3]))
-    col = lambda c1, c2: itemgetter(c1, c2)(palette)
-    
-    species = list(df[cats].unique())
-    species.reverse()
-    
-    fig, axs = plt.subplots(1, 3, figsize=(16, 6))
-    fig.suptitle(f"Class distributions for {var}")
-    
-    for i, (cla, cols) in enumerate(zip(species, combinations(colours, 2))):
-        histplot(df[df[cats] != cla], x=var, multiple="stack", hue=cats, bins=15, ax=axs[i], palette=col(*cols))
     plt.show()
 
 
